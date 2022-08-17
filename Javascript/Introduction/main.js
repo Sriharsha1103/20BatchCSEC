7c2ab5300f64d846fa9f7dc60d1bd8480a4cec0f
Sriharsha1103 <sriharsha1103@gmail.com>
2022-08-17 13:11:15 +0530

    Functions

--- Javascript/Introduction/main.js
+++ Javascript/Introduction/main.js
@@ -60,3 +60,132 @@ let var3 = "15";
 console.log(var2 != var3)
 console.log(var2 !== var3)
 
+//Control Statements and functions
+/* 
+if, if-else, else-if, switch, while, for, do-while, return, break,continue
+*/
+//Print numbers from 1 to 10
+/* Anonymous Funtion in function expression */
+let myPrint = function (n = 10){
+    for(let i=1; i<=n; i++){
+            console.log(i);
+    }
+}
+//let myPrint = print;
+myPrint(30);
+console.log(myPrint);
+//print(5);
+//Prime or not
+
+
+
+
+/* IIFE - Immediate Invocation Function Expression */
+
+/* (function (){}()) */
+(function (){
+    console.log("Hi");
+}());
+
+console.log(function (n = 8){
+    let flag = 1;
+    for(let i = 2; i< n; i++){
+        if(n%i === 0)
+            flag = 0;
+    }
+    if(flag)
+    console.log("Prime number");
+    else
+    console.log("it is not prime number");
+    return flag;
+}());
+
+/* const result = isPrime(14);
+if(result)
+    console.log("Prime number");
+else
+    console.log("it is not prime number"); */
+
+let n = 10;
+let flag = 1;
+for(let i = 2; i< n; i++){
+    if(n%i === 0)
+        flag = 0;
+}
+if(flag)
+    console.log("Prime number");
+else
+    console.log("it is not prime number");
+
+/* HOF */
+
+function print(){
+    console.log("Message");
+}
+function print1(){
+    console.log("MyMessage");
+}
+
+function display(myFunc){
+    myFunc();
+}
+
+display(print);
+display(print1);
+
+/* Callback Function */
+setTimeout(function (){
+    console.log("Hi");
+}, 3000);
+/* Arrow Function */
+const A = () => {
+    console.log("Function A");
+}
+
+const myFib = (n) => {
+
+
+}
+myFib(24);
+
+/* call, bind, apply */
+
+const Person = {
+    name: 'Harsha',
+    phoneNumber: 9908055084,
+
+    print : function (){
+        console.log(name, phoneNumber);
+    }
+};
+
+console.log(Person.name);
+
+
+/*
+    const A = function (){
+      return  console.log("Function A");
+    }
+
+ */
+
+
+
+/*Callback function 
+    function A(function (){
+
+    }){
+
+    }
+*/
+
+setInterval(function (){},3000);
+    /* Types of functions
+    1. Normal Function
+    2. Function Expression
+    3. Anonymous Function/ function expression
+    4. IIFE
+    5. Higher Order Function
+    6. Arrow Function
+    7.Callback Function
+    */
